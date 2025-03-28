b13108965d6fc4f7da84a62403e5b4fc7bd9e2f7
anamnikhilesh@gmail.com <anamnikhilesh@gmail.com>
2025-03-28 00:22:30 -0700

    update uf

--- union_find.py
+++ union_find.py
@@ -4,6 +4,7 @@ class UnionFind: # Disjoint Set Union or Union Find by Rank
     def __init__(self, n):
         self.parentNode = list(range(n))  # Each node starts as its own parent
         self.treeRank = [1] * n           # Each tree starts with rank 1
+        self.size = [1] * n  # Track the size of each component
 
     def find(self, node):
         # Path Compression: Point each node directly to its root
@@ -19,25 +20,32 @@ class UnionFind: # Disjoint Set Union or Union Find by Rank
             # Union by Rank: Attach the smaller tree under the larger one
             if self.treeRank[rootA] > self.treeRank[rootB]:
                 self.parentNode[rootB] = rootA
+                self.size[rootA] += self.size[rootB]
             elif self.treeRank[rootA] < self.treeRank[rootB]:
                 self.parentNode[rootA] = rootB
+                self.size[rootB] += self.size[rootA]
             else:
                 if random.choice([True, False]):  # Choose randomly between True (rootA) or False (rootB)
                     self.parentNode[rootB] = rootA
                     self.treeRank[rootA] += 1  # Increase the rank of rootA when attaching rootB to rootA
+                    self.size[rootA] += self.size[rootB]
                 else:
                     self.parentNode[rootA] = rootB
                     self.treeRank[rootB] += 1  # Increase the rank of rootB when attaching rootA to rootB
-
+                    self.size[rootB] += self.size[rootA]
+    
+    def getSize(self, node):
+        return self.size[self.find(node)]
+    
     def getSetsInfo(self):
         # This function returns a dictionary with root nodes as keys
-        # and corresponding height (rank) and members (list of nodes) of each set as values.
+        # and corresponding rank and members (list of nodes) of each set as values.
         set_info = {}
         for i in range(len(self.parentNode)):
             root = self.find(i)
             # Initialize the set entry if not already present
             if root not in set_info:
-                set_info[root] = {'height': self.treeRank[root], 'members': []}
+                set_info[root] = {'size': self.size[root],'rank': self.treeRank[root], 'members': []}
             # Add the current node to the list of members for the set
             set_info[root]['members'].append(i)
         return set_info
